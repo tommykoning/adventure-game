c0c37b0deea931fd146b3a30cebdac3d35b6740f
tommy koning <99039195@mydavinci.nl>
2016-11-03 11:14:49 +0100

    css 50%

--- game.js
+++ game.js
@@ -1,3 +1,20 @@
-var x = 1;
+function openscreen(evt, screenname) {
 
-console.log (5 + 5);
+var i, tabLink, screen;
+
+	//get all hidden
+	screen = document.getElementsByClassName("screen");
+	for (var i = 0; i < screen.length; i++) {
+		screen[i].style.display = "none"; }
+	
+	//get active removed
+	tabLink = document.getElementsByClassName("tabLink");
+	for (var i = 0; i < tabLink.length; i++) {
+	 	tabLink[i].className = tabLink[i].className.replace ("active", "");
+		}
+	
+	//make visible
+	document.getElementById(screenname).style.display = "block"; 
+	evt.currentTarget.className += "active";
+	}
+
