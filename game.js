7c37a65ffb27ee4433d28ceb53561279efd4a13f
tommy koning <99039195@mydavinci.nl>
2016-11-16 18:12:44 +0100

    typewrighter works ... finnaly

--- game.js
+++ game.js
@@ -18,5 +18,18 @@
 			evt.currentTarget.className += " active"; }
 	
 		// auto click on but-2
-			document.getElementById("but-2").click();
+			document.getElementById("but-1").click();
 
+//typewrighter
+	//verhaal
+	var story = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 	fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+	
+	//zorgt ervoor dat het gesplistst word 
+	var cut = story.split('');
+	var Count = 0;
+
+	//het typewrighter effect
+	setInterval(function(){ 
+		if (Count < cut.length) { 
+			document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + cut[Count]};
+			Count ++;},50);
