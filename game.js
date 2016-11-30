0c60c35a58db49607fdf264bc108d6fa5b18ccb9
tommy koning <99039195@mydavinci.nl>
2016-11-30 11:36:26 +0100

    typewrighter begins when button is pressed

--- game.js
+++ game.js
@@ -1,57 +1,84 @@
 // tabs
-	function openscreen(evt, screenname) {
+function openscreen(evt, screenname) {
+
+	var i, tabLink, screen;
+
+	//get all hidden
+	screen = document.getElementsByClassName("screen");
+	for (var i = 0; i < screen.length; i++) {
+		screen[i].style.display = "none"; 
+	}
 	
-		var i, tabLink, screen;
+	//get active removed
+	tabLink = document.getElementsByClassName("tabLink");
+	for (var i = 0; i < tabLink.length; i++) {
+	 	tabLink[i].className = tabLink[i].className.replace (" active", "");
+	 }
 	
-		//get all hidden
-			screen = document.getElementsByClassName("screen");
-			for (var i = 0; i < screen.length; i++) {
-				screen[i].style.display = "none"; }
-		
-		//get active removed
-			tabLink = document.getElementsByClassName("tabLink");
-			for (var i = 0; i < tabLink.length; i++) {
-			 	tabLink[i].className = tabLink[i].className.replace (" active", "");}
-		
 	//make visible
-		document.getElementById(screenname).style.display = "block"; 
-		evt.currentTarget.className += " active"; }
-	
-	// auto click on but-2
-		document.getElementById("but-2").click();
+	document.getElementById(screenname).style.display = "block"; 
+	evt.currentTarget.className += " active"; 
+}
+
+// auto click on but-2
+document.getElementById("but-2").click();
 
 //start game
-	function start() { 
-		document.getElementById("but-1").click()}
-
+function start() { 
+	start = noop
+	document.getElementById("but-1").click();
+	document.getElementById("pic").src = "https://d.ibtimes.co.uk/en/full/1419300/empty-spaces-abandoned-places-johnny-joo-urbexus.jpg";
+}
 
 //restard
-	function restart() { 
-		location.reload();}
+function restart() { 
+	location.reload();
+}
 
 //typewrighter
-	//verhaal
-		var story = 'I had acquired the money to attend the finest school in Zinland but that was just the beginning. While I would love to say Zinkov Preparatory Academy embraced me with open arms, the truth of the matter is I was not the finest student. In order to gain admission, I had to endure rigorous physical and academic testing before they would grant me access to their legendary halls of learning. The first of those struggles were my parents. My father, a simple butcher, was a small man with a fierce temper. Of all the things I learned from my father, the one that sticks with me most has to be: ';
-	
-	//zorgt ervoor dat het gesplistst word 
-		var cut = story.split('');
-		var Count = 0;
+//verhaal
+var startgame = document.getElementsByClassName("startgame");
 
-	//het typewrighter effect
-		setInterval(function(){ 
-			if (Count < cut.length) { 
-				document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + cut[Count] };
-				Count ++;} ,50);
+var story = '(year: 8102. month: may. day: 12.) i woke up my left eye hurts i see trough a faded light im trapped in a cell there is a lot of noice people yelling and screaming when i remember, i was a soldier in a prived militia we got a order to assasinate a govenour and "clean up" everything and everyone who could give any kind of information about what happend. my memory is hazy about what happend on the mission but i remember a child i dont know what happend to him/her/it but i assume we: ';
+
+//zorgt ervoor dat het gesplistst word 
+var snij = story.split('');
+var count = 0;
+
+//typewrighter effect
+function typewrighter() {
+	setInterval (function (){ 
+		if (count < snij.length) { 
+			document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + snij[count]; count ++ ;
+		}		
+	} ,5);
+}
+
 
 //foto's vergroten/verkleinen (a.k.a modal image)
-	//vergrooten 
-	var img = document.getElementById("pic");
-	
-	img.onclick = function(){
-		document.getElementById("modal").style.display = "block"; 
-		document.getElementById("foto-groot").src = this.src; };
-	
+//vergrooten 
+var img = document.getElementById("pic");
+
+img.onclick = function(){
+	document.getElementById("modal").style.display = "block"; 
+	document.getElementById("foto-groot").src = this.src; 
+};
+
 //verkleinen
-		function verklein() { 
-			document.getElementById("modal").style.display = "none";
-			console.log('sluiten'); }
+function verklein() { 
+	document.getElementById("modal").style.display = "none";
+	console.log('sluiten'); 
+}
+
+//choices
+//chouce 1
+var but1 = document.getElementById("menu-1")
+
+but1.onclick = function() {
+ story += 'test';
+ console.log(test)
+}
+
+//noop 
+function noop() {};
+
