9f903ae281502536fb70863965098616f7300921
tommy koning <99039195@mydavinci.nl>
2016-12-07 16:37:23 +0100

    keuzes fixed (not done fixed)

--- game.js
+++ game.js
@@ -30,6 +30,7 @@ function start() {
 	document.getElementById("pic").src = "https://d.ibtimes.co.uk/en/full/1419300/empty-spaces-abandoned-places-johnny-joo-urbexus.jpg";
 }
 
+
 //restard
 function restart() { 
 	location.reload();
@@ -39,19 +40,30 @@ function restart() {
 //verhaal
 var startgame = document.getElementsByClassName("startgame");
 
-var story = '(year: 8102. month: may. day: 12.) i woke up my left eye hurts i see trough a faded light im trapped in a cell there is a lot of noice people yelling and screaming when i remember, i was a soldier in a prived militia we got a order to assasinate a govenour and "clean up" everything and everyone who could give any kind of information about what happend. my memory is hazy about what happend on the mission but i remember a child i dont know what happend to him/her/it but i assume we: ';
+var story = '(year: 8102. month: may. day: 12.) i woke up my left eye hurts i see trough a faded light im trapped in a cell there is a lot of noice people yelling and screaming then i remember, i was a soldier in a prived militia we got a order to assasinate a govenour and "clean up" everything and everyone who could give any kind of information about what happend. my memory is hazy about what happend on the mission but i remember a child i dont know what happend to him/her/it but i assume we: ';
 
 //zorgt ervoor dat het gesplistst word 
 var cut = story.split('');
 var count = 0;
 
+
+
 //typewrighter effect
+var time = 50;
 function typewrighter() {
 	setInterval (function (){ 
 		if (count < cut.length) { 
-			document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + cut[count]; count ++ ;
+			document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + cut[count]; 
+			count ++ ;
+
+			//skip
+			function skip() {
+				if (count < cut.length) {
+					time = 5;
+				}
+			}
 		}		
-	} ,5);
+	} ,time);
 }
 
 
@@ -69,10 +81,30 @@ function verklein() {
 	document.getElementById("modal").style.display = "none"; 
 }
 
+
 //choices
-//chouce 1
+var kies1 = 'test';
+var count2 = 0;
+var level = 1;
+//keuzes verschijnen na dat typrwrichter klaar is 
+setInterval ( function() {
+	if (count == cut.length) {
+		setInterval ( function() {
+			if (count2 < level) {
+				document.getElementById('menu1').innerHTML = document.getElementById('menu1').innerHTML + kies1;
+				document.getElementById('menu2').innerHTML = document.getElementById('menu2').innerHTML + kies1;
+				document.getElementById('menu3').innerHTML = document.getElementById('menu3').innerHTML + kies1;
+				count2 ++;
+			}
+		}, time);
+	}
+ }, time);
 
+//choice 1
+document.getElementById('menu1').onclick = function() {
+	story + kies1
+}
 
-//noop 
+//no operation on point (noop)
 function noop() {};
 
